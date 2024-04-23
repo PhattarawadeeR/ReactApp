eff2de58f9838eed828bed4e93a4261c3becc3ff
PhattarawadeeR <153511627+PhattarawadeeR@users.noreply.github.com>
2024-04-23 15:47:36 +0700

    update http to https

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,20 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-const BASE_URL = 'http://us-central1-skooldio-react-hooks.cloudfunctions.net'
+const BASE_URL = 'https://us-central1-skooldio-react-hooks.cloudfunctions.net';
 
 export const useAPI = (path) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    useEffect(() => {
-        setLoading(true)
-        fetch (BASE_URL + path)
-        .then((resp) => resp.json())
-        .then((data) => {
-            setData(data);
-            setLoading(false);
-        });
-    }, []);
-    return { data, loading };
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    fetch(BASE_URL + path)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+  return { data, loading };
 };
 
-export default useAPI; 
+export default useAPI;
